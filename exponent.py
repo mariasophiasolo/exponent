4b82c999d2f68e9b73df931105ffdf6750f7d6c2
Maria Sophia M. Solo <solomariasophia@gmail.com>
2024-01-28 00:13:36 +0800

    working

--- exponent.py
+++ exponent.py
@@ -13,11 +13,16 @@ def exponent(base, exp):
     # return the final result
     return result
 
-case1_base_value = 5
-case1_exponent_value = 4
+case1_base_value = 2
+case1_exponent_value = 5
+
+case2_base_value = 5
+case2_exponent_value = 4
 
 # call the exponent function with the provided base and exponent values
-result = exponent(case1_base_value, case1_exponent_value)
+result1 = exponent(case1_base_value, case1_exponent_value)
+result2 = exponent(case2_base_value, case2_exponent_value)
 
 # display the result using f-string formatting
-print(f"{case1_base_value} raised to the power of {case1_exponent_value} is: {result}")
+print(f"{case1_base_value} raised to the power of {case1_exponent_value} is: {result1}")
+print(f"{case2_base_value} raised to the power of {case2_exponent_value} is: {result2}")
